ba1b2c7f84db0590abe91169ec0d996d792f0a8a
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2025-02-08 13:48:21 +0530

    add:img commands

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'v0.blob.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+        },
+      ],
+    },
+  }
+  
+  
