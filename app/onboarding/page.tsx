2e40d0c4561f87edfb6b6f0add707e40d33b4e4e
mrcoder57 <tiwariji2300@gmail.com>
2025-02-08 18:33:02 +0530

    fix:responsiveness

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Onboardpage = () => {
   return (
-    <div>Onboardpage</div>
+    <div className=' h-screen flex justify-center items-center '>Onboardpage</div>
   )
 }
 
