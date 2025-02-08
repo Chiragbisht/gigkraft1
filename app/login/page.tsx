7d9e3356694c938885c86a86d94147972907070a
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2025-02-08 13:48:42 +0530

    Add: footer

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,19 @@
 import LoginCard from '@/components/loginPage/loginCard'
 import React from 'react'
+import Footer from '@/components/footer'
 
 const LoginPage = () => {
   return (
-    <div className=' flex items-center justify-center h-screen'>
-        <LoginCard/>
+    <div className=" ">
+      {/* Main Content */}
+      <div className="flex flex-col items-center justify-center mt-12 mb-10 flex-grow">
+        <LoginCard />
+      </div>
+      
+      {/* Footer Stays at Bottom */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
