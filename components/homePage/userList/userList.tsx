a3f28725d91d99a8959216999009e26b67feba58
mrcoder57 <tiwariji2300@gmail.com>
2025-02-12 20:38:25 +0530

    fix: add box shadow

--- components/homePage/userList/userList.tsx
+++ components/homePage/userList/userList.tsx
@@ -26,6 +26,7 @@ export default function UserList() {
             key={page}
             variant={page === 1 ? "default" : "outline"}
             className="w-10 h-10"
+            style={{ boxShadow: "0px 4px 45px 0px #0000001F" }}
           >
             {page}
           </Button>
