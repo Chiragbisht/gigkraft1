f123171ef6b0e1830ba9dcb9a97214851594dd26
anil yadav <anilyadav6803@gmail.com>
2025-02-21 01:06:54 +0530

    Fix: button size

--- components/metaMaskPage/metaMask.tsx
+++ components/metaMaskPage/metaMask.tsx
@@ -37,9 +37,7 @@ else{
     <div>
      
       <button 
-      className="text-black bg-green-700 hover:bg-green-400 focus:outline-none focus:ring-4 focus:ring-gray-300
-       font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700
-        dark:focus:ring-gray-700 dark:border-gray-700"
+      className="text-black p-2 rounded-sm w-36 bg-green-700 hover:bg-green-400"
       onClick={connectWallet} > Connect wallet</button>
    
     </div>
