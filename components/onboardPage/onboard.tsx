80f526ea37de1517c657ef77ed63b427ed40e00e
mrcoder57 <tiwariji2300@gmail.com>
2025-02-22 20:56:22 +0530

    fix: alignment

--- components/onboardPage/onboard.tsx
+++ components/onboardPage/onboard.tsx
@@ -1,15 +1,13 @@
-import { motion } from "motion/react"
-import React from 'react'
-import MultiStepForm from './multistepForm/multiStepForm'
+import { motion } from "motion/react";
+import React from "react";
+import MultiStepForm from "./multistepForm/multiStepForm";
 
 const OnBoard = () => {
   return (
-    <div className=' flex items-center justify-center h-full '>
-      
-        <MultiStepForm />
-        
+    <div className=" flex items-center justify-center h-full ">
+      <MultiStepForm />
     </div>
-  )
-}
+  );
+};
 
-export default OnBoard
+export default OnBoard;
