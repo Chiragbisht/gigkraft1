b52551e933f252e6ef069f8affd27c1b09b18042
mrcoder57 <tiwariji2300@gmail.com>
2025-02-27 21:10:18 +0530

    fix:  add min height

--- app/search/jobs/page.tsx
+++ app/search/jobs/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SearchPage = () => {
   return (
-    <div className=" w-full h-full py-10">
+    <div className=" w-full h-full min-h-screen py-10">
       <div className=" max-w-7xl mx-auto h-full">
         <SearchJobs />
       </div>
