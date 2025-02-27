cfcb8f810fcdbe1ade8636e7b2c4a3844d193b18
mrcoder57 <tiwariji2300@gmail.com>
2025-02-27 13:15:36 +0530

    feature: create page for google Auth

---
+++ app/auth/google/callback/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+
+const GoogleCallback = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+  console.log(code);
+  useEffect(() => {
+    const exchangeCodeForToken = async () => {
+      if (!code) return;
+
+      console.log("Attempting to exchange code for token:", code);
+
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/auth/google/callback?code=${code}`,
+          { withCredentials: true }
+        );
+
+        console.log("Full Response from Backend:", response);
+
+        const { token } = response.data; // Ensure the backend actually sends this
+
+        if (token) {
+          console.log("Access token received:", token);
+          localStorage.setItem("token", token);
+          router.push("/home");
+        } else {
+          console.error("No token in response:", response.data);
+        }
+      } catch (error: any) {
+        console.error(
+          "Error exchanging code for token:",
+          error.response?.data || error
+        );
+      }
+    };
+
+    exchangeCodeForToken();
+  }, [code, router]);
+
+  return (
+    <div className=" h-screen flex items-center justify-center">
+      <p>Processing Google login...</p>
+    </div>
+  );
+};
+
+export default GoogleCallback;
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,23 +1,18 @@
 "use client";
-import axios from "axios";
-import { Target, DollarSign, Rocket, Maximize2, Key } from "lucide-react";
+;
+import { useAuthStore } from "@/zustand/nameStore";
+import { useEffect } from "react";
 
 export default function FeaturesSection() {
-  const fetchJobs = async () => {
-    try {
-      const { data } = await axios.get(`http://localhost:3000/jobs`, {
-        withCredentials: true,
-      });
-      console.log(data);
-      return data;
-      
-    } catch (error: any) {
-      throw new Error(error.response?.data?.message || "Jobs Fetch Failed");
-    }
-  };
+  const { token } = useAuthStore();
+
+  useEffect(() => {
+    // console.log("Zustand token updated:", token);
+  }, [token]);
+
   return (
     <section className="py-16 px-4 md:py-24">
-      <button onClick={fetchJobs}>Get Started</button>
+      {/* <p>Token: {token ? token : "No token found"}</p> */}
     </section>
   );
 }
