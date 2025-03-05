794dcf4d9ff0ba7d369afdd01af2899121aa8f2b
anil yadav <anilyadav6803@gmail.com>
2025-03-05 13:56:32 +0530

    feature : chats page

---
+++ app/chats/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ChatLayout from "@/components/chatPage/chat-layout"
+
+const chats = () => {
+  return (
+    <div className=' '>
+        <ChatLayout />
+    </div>
+  )
+}
+
+export default chats
+
+
+
+
+
+
+
+
+
+
+
+
